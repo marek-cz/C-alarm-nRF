99688925659cc58514f65bf530528ef002a01fda
Marek <m-czekala@o2.pl>
2022-05-02 13:04:36 +0200

    Struct do obslugi gpio

--- Program_nRF/main.c
+++ Program_nRF/main.c
@@ -8,18 +8,29 @@
 #include <avr/io.h>
 #include <avr/delay.h>
 
+typedef struct  
+{
+	uint8_t PORTx;
+	uint8_t DDRx;
+	uint8_t PINx;
+	uint8_t pinNumber;
+}gpioPin_t;
 
 int main(void)
 {
-	
+	/*
 	DDRD	|= (1<<7);
 	PORTD	|= (1<<7);
+	*/
+	gpioPin_t led2 = { PORTD, DDRD, PIND, 7 };
+	led2.DDRx |= (1 << led2.pinNumber);
+	led2.PORTx |= (1 << led2.pinNumber);
 	
     /* Replace with your application code */
     while (1) 
     {
 		_delay_ms(500);
-		PORTD ^= (1<<7);
+		led2.PORTx ^= (1 << led2.pinNumber);
     }
 }
 
