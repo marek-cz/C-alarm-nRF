da4f59b34c0a3cfe9832a6132a6b395b798a6870
Marek <m-czekala@o2.pl>
2022-05-24 18:14:45 +0200

    Zdefiniowanie w programie wszystkich wykorzystywanych pinow uC

--- Program_nRF/main.c
+++ Program_nRF/main.c
@@ -14,14 +14,28 @@
 int main(void)
 {
 	
+	gpioPin_t led1 = { .PORTx = &PORTD, .DDRx = &DDRD, .PINx = &PIND, .pinNumber = PD6 };
 	gpioPin_t led2 = { .PORTx = &PORTD, .DDRx = &DDRD, .PINx = &PIND, .pinNumber = PD7 };
 	gpioPin_t buzzer = { .PORTx = &PORTD, .DDRx = &DDRD, .PINx = &PIND, .pinNumber = PD4 };
 	gpioPin_t alarmLed = { .PORTx = &PORTD, .DDRx = &DDRD, .PINx = &PIND, .pinNumber = PD5 };
+	gpioPin_t extInt0 = { .PORTx = &PORTB, .DDRx = &DDRB, .PINx = &PINB, .pinNumber = PB0 };
+	gpioPin_t extInt1 = { .PORTx = &PORTD, .DDRx = &DDRD, .PINx = &PIND, .pinNumber = PD3 };
+	// linie SPI - nRF
+	gpioPin_t CE = { .PORTx = &PORTB, .DDRx = &DDRB, .PINx = &PINB, .pinNumber = PB1 };
+	gpioPin_t nSS = { .PORTx = &PORTB, .DDRx = &DDRB, .PINx = &PINB, .pinNumber = PB2 };
+	gpioPin_t mosi = { .PORTx = &PORTB, .DDRx = &DDRB, .PINx = &PINB, .pinNumber = PB3 };
+	gpioPin_t miso = { .PORTx = &PORTB, .DDRx = &DDRB, .PINx = &PINB, .pinNumber = PB4 };
+	gpioPin_t sclk = { .PORTx = &PORTB, .DDRx = &DDRB, .PINx = &PINB, .pinNumber = PB5 };	
 	
 	// Konfiguracja pinow jako wyjscia:
+	setPinOutput(led1);
 	setPinOutput(led2);
 	setPinOutput(buzzer);
 	setPinOutput(alarmLed);
+	setPinOutput(sclk);
+	setPinOutput(mosi);
+	setPinOutput(nSS);
+	
 	
 	// stan poczatkowy
 	setPin(led2);
