a539d1fcc38c7338f12b1235a6ee585663644a7b
Marek <m-czekala@o2.pl>
2022-05-26 19:22:41 +0200

    Dodanie funkcji realizujacych inicjalizacje i transmisje SPI

--- Program_nRF/SPI/SPI.c
+++ Program_nRF/SPI/SPI.c
@@ -2,36 +2,51 @@
  * SPI.c
  *
  * Created: 2022-05-22 14:07:33
- *  Author: Marek
+ *  Author: P.Czekala
  */ 
 
 #include <avr/io.h>
 #include "SPI.h"
 
-void spiInit(void)
+void spiInit(void) //SPI initialization
 {
 	//TODO
+	SPI_DDR |= (1<<MOSI)|(1<<SCK)|(1<<SS);
+	SET_SS;//high state on SS line
+	//SPCR &= SPI_CLOCK_POLARITY; // clock polarity bit cleared
+	//SPCR &= SPI_CLOCK_PHASE;// clock phase bit cleared
+	SPCR |= SPI_DATA_ORDER; // the LSB of the data word is transmitted first.
+	SPCR &= SPI_CLOCK_POLARITY & SPI_CLOCK_PHASE;// clock polarity and clock phase bits cleared
+	SPCR |= SPI_DATA_ORDER | (1<<MSTR) | SPI_SPCR_SPE | (SPR0)| SPI_INTER_ENABLED;
+	//SPCR |= SPI_SPCR_SPE;// SPI enable
 }
 
-static void spiWriteByte( uint8_t data )
+
+uint8_t spiTransreceive(uint8_t data)
 {
-	//TODO
+	//transmit and/or receive 1 byte of data
+	SPDR = data;
+	while(!(SPSR & (1<<SPIF)));
+	return SPDR;
+	
 }
-
-static uint8_t spiReadByte( void )
-{
-	//TODO
-}
-
-void spiWrite( uint8_t data, gpioPin_t nSS )
+void spiWrite( uint8_t* data, gpioPin_t nSS,int N )// writing data to SPI
 {
 	//TODO
 	clearPin(nSS);
-	// write to reg
+	for (uint8_t it=0;it<N;++it)
+	{
+		spiTransreceive(data[it]);
+	}
 	setPin(nSS);
 }
 
-uint8_t spiRead( gpioPin_t nSS )
+uint8_t spiRead( gpioPin_t nSS )// reading data from SPI
 {
-	//TODO
+	
+	clearPin(nSS);
+	uint8_t data = spiTransreceive(0x00);
+	setPin(nSS);
+	return data;
+	
 }
--- Program_nRF/SPI/SPI.h
+++ Program_nRF/SPI/SPI.h
@@ -2,7 +2,7 @@
  * SPI.h
  *
  * Created: 2022-05-22 14:06:18
- *  Author: Marek
+ *  Author: P. Czekala
  */ 
 
 
@@ -11,8 +11,27 @@
 
 #include "../GPIO/GPIO.h"
 
+// SPI I/O PINS
+#define SPI_DDR DDRB
+#define SPI_PORT PORTB
+#define MOSI PB3
+#define SCK PB5
+#define SS PB2
+// SPI nSS SET/CLEAR MACRO
+#define SET_SS SPI_PORT |=(1<<SS)
+#define CLR_SS SPI_PORT &=~(1<<SS)
+// SPI CONFIG BITS
+#define SPI_SPCR_SPE PB6
+#define SPI_CLOCK_POLARITY ~(1<<3)
+#define SPI_CLOCK_PHASE ~(1<<2)
+#define SPI_DATA_ORDER 1<<5
+#define SPI_INTER_ENABLED 1<<7
+
+//######################################################################
+
 void spiInit(void);
-void spiWrite( uint8_t data, gpioPin_t nSS );
+void spiWrite( uint8_t* data, gpioPin_t nSS, int N );
 uint8_t spiRead( gpioPin_t nSS);
+uint8_t spiTransreceive( uint8_t data);
 
 #endif /* SPI_H_ */
